d4570999fefc9b0591b56e5ff86e71cce04bb2ba
Patrick <pseminatore@gmail.com>
2022-08-29 21:30:09 -0700

    Created model and evaluations strategies

---
+++ evaluation.py
@@ -0,0 +1,47 @@
+import nfl_data_py as nfl
+import pandas as pd
+import matplotlib.pyplot as plt
+from os.path import exists
+import xgboost as xgb
+from model import *
+
+
+def mov_correlation(rebuild_model=False, verbose=False):
+    if not exists('model.txt') or rebuild_model:
+        model = build_model(verbose=verbose)
+    else:
+        model = xgb.XGBRegressor()
+        model.load_model("model.txt")
+    
+    df = nfl.import_pbp_data([2021], cache=True, downcast=False, columns=['desc'])
+
+        
+    df = nfl.import_pbp_data([2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021], cache=True, downcast=False, columns=['passer_player_id', 'passer_player_name', 'season', 'game_id', 'pass', 'epa', 'qb_epa', 'wpa', 'cp', 'cpoe', 'complete_pass','air_yards', 'xyac_mean_yardage', 'receiver_player_name', 'pass_location', 'posteam_score_post', 'defteam_score_post'])
+    df = df.query('`pass` == 1')
+    df = df.dropna(subset=['passer_player_id', 'receiver_player_name', 'pass_location'])
+    df[['cp', 'cpoe', 'air_yards', 'xyac_mean_yardage']] = df[['cp', 'cpoe', 'air_yards', 'xyac_mean_yardage']].fillna(0)
+    df = df.query('air_yards > -15 and air_yards < 75')
+    df['xCompletion'] = df.apply(lambda row: 1 if row['complete_pass'] == 1 or row['cp'] > 0.5 else 0, axis=1)
+    df['xYards'] = df.apply(lambda row: row['air_yards'] + row['xyac_mean_yardage'] if row['xCompletion'] == 1 else 0, axis=1)
+    df = df[['passer_player_id', 'passer_player_name', 'season', 'game_id', 'epa', 'qb_epa', 'wpa', 'xCompletion', 'xYards', 'pass', 'posteam_score_post', 'defteam_score_post']].groupby(by=['passer_player_id', 'passer_player_name', 'season', 'game_id']).agg(epa=('epa', 'sum'), qb_epa=('qb_epa', 'sum'), wpa=('wpa', 'sum'), xCompletion=('xCompletion', 'sum'), xYards=('xYards', 'sum'), att=('pass', 'sum'), posteam_score=('posteam_score_post', 'max'), defteam_score=('defteam_score_post', 'max')).reset_index()
+    df['MoV'] = df['posteam_score'] - df['defteam_score']   
+    df['xCompPer'] = df['xCompletion'] / df['att']
+    X = df[['wpa', 'xCompPer', 'xCompletion', 'xYards', 'att']]
+    xEPA_df = model.predict(X) 
+    df['xEPA'] = xEPA_df.tolist()   
+    r2 = r2_score(df['MoV'], df['xEPA'])
+    rmse = np.sqrt(mean_squared_error(df['MoV'], df['xEPA']))
+    if verbose:
+        print("R squared: %f" % (r2))
+        print("RMSE: %f" % rmse)
+        plt.scatter(x=df['MoV'], y=df['xEPA'])
+        plt.ylabel('xEPA')
+        plt.xlabel('Margin of Victory')
+        plt.title(f"RMSE: {rmse} | R Squared: {r2}")
+        plt.show()
+    return 0    
+
+
+if __name__ == '__main__':
+    pd.set_option('mode.chained_assignment', None)
+    mov_correlation(rebuild_model=False, verbose=True)
---
+++ model.py
@@ -0,0 +1,102 @@
+from pyexpat.errors import XML_ERROR_INCOMPLETE_PE
+import nfl_data_py as nfl
+import pandas as pd
+import xgboost as xgb
+import numpy as np
+import sklearn
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+import matplotlib.pyplot as plt
+
+
+## TODO - prove that pred EPA is more predictive of either next season or next games EPA than prev EPA
+def build_model(verbose=False, file_path='model.txt'):
+    #nfl.cache_pbp([2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021], downcast=False)
+    df = nfl.import_pbp_data([2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021], cache=True, downcast=False, columns=['passer_player_id', 'passer_player_name', 'season', 'game_id', 'pass', 'epa', 'qb_epa', 'wpa', 'cp', 'cpoe', 'complete_pass','air_yards', 'xyac_mean_yardage', 'receiver_player_name', 'pass_location'])
+    df = df.query('`pass` == 1')
+    df = df.dropna(subset=['passer_player_id', 'receiver_player_name', 'pass_location'])
+    df[['cp', 'cpoe', 'air_yards', 'xyac_mean_yardage']] = df[['cp', 'cpoe', 'air_yards', 'xyac_mean_yardage']].fillna(0)
+    df = df.query('air_yards > -15 and air_yards < 75')
+    df['xCompletion'] = df.apply(lambda row: 1 if row['complete_pass'] == 1 or row['cp'] > 0.5 else 0, axis=1)
+    df['xYards'] = df.apply(lambda row: row['air_yards'] + row['xyac_mean_yardage'] if row['xCompletion'] == 1 else 0, axis=1)
+    df = df[['passer_player_id', 'passer_player_name', 'season', 'game_id', 'epa', 'qb_epa', 'wpa', 'xCompletion', 'xYards', 'pass']].groupby(by=['passer_player_id', 'passer_player_name', 'season', 'game_id']).sum().reset_index()
+    
+    y = df['qb_epa']
+    df['xCompPer'] = df['xCompletion'] / df['pass']
+    X = df[['wpa', 'xCompPer', 'xCompletion', 'xYards', 'pass']]
+    data_dmatrix = xgb.DMatrix(data=X, label=y)
+    
+
+    # estimator = xgb.XGBRegressor(objective ='reg:squarederror', nthread=4, seed=42)
+    # parameters = {
+    #     'max_depth': range (2, 10, 1),
+    #     'n_estimators': range(60, 220, 40),
+    #     'learning_rate': [0.1, 0.01, 0.05]
+    # }
+    # grid_search = GridSearchCV(
+    #     estimator=estimator,
+    #     param_grid=parameters,
+    #     scoring = 'r2',
+    #     n_jobs = 10,
+    #     cv = 10,
+    #     verbose=True
+    # )
+    # grid_search.fit(X, y)
+    # print('best estimator: %s' % grid_search.best_estimator_)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+    xg_reg = xgb.XGBRegressor(objective='reg:squarederror', base_score=0.5, booster='gbtree', colsample_bylevel=1,
+             colsample_bynode=1, colsample_bytree=1, enable_categorical=False,
+             gamma=0, gpu_id=-1, importance_type=None,
+             interaction_constraints='', learning_rate=0.05, max_delta_step=0,
+             max_depth=3, min_child_weight=1,
+             monotone_constraints='()', n_estimators=140, n_jobs=4, nthread=4,
+             num_parallel_tree=1, predictor='auto', random_state=42,
+             reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=42,
+             subsample=1, tree_method='exact', validate_parameters=1,
+             verbosity=None)
+    xg_reg.fit(X_train,y_train)
+    preds = xg_reg.predict(X_test)
+    r2 = r2_score(y_test, preds)
+    rmse = np.sqrt(mean_squared_error(y_test, preds))
+    if verbose:
+        print("R squared: %f" % (r2))
+        print("RMSE: %f" % rmse)
+        plt.scatter(x=y_test, y=preds)
+        plt.ylabel('Predicted EPA')
+        plt.xlabel('Actual EPA')
+        plt.title(f"RMSE: {rmse} | R Squared: {r2}")
+        plt.show()
+    #xgb.plot_importance(xg_reg)
+    #plt.rcParams['figure.figsize'] = [5, 5]
+    # params = {"objective":"reg:linear",'colsample_bytree': 0.3,'learning_rate': 0.1,
+    #             'max_depth': 5, 'alpha': 10}
+
+    # cv_results = xgb.cv(dtrain=data_dmatrix, params=params, nfold=3,
+    #                 num_boost_round=50,early_stopping_rounds=10,metrics="rmse", as_pandas=True, seed=123)
+    # rmse = (cv_results["test-rmse-mean"]).tail(1).values.tolist()[0]
+    xg_reg.save_model(file_path)
+    return xg_reg
+    
+    
+    ### TODO -- Sum these by season and compare to next
+    #df = df.groupby(by=['passer_player_id', 'passer_player_name', 'season']).agg(wpa=('wpa', 'sum'), xCompPer=('xCompPer', np.mean), xCompletion=('xCompletion', 'sum'), xYards=('xYards', 'sum'), passes=('pass', 'sum'), qb_epa=)
+    # prev_act_y = df.query('season == 2020')['qb_epa']
+    # next_act_y = df.query('season == 2021')['qb_epa']
+    # prev_act_x = df.query('season == 2020')[['passer_player_id', 'passer_player_name', 'wpa', 'xCompPer', 'xCompletion', 'xYards', 'pass', 'qb_epa']]
+    # next_act_x = df.query('season == 2021')[['passer_player_id', 'passer_player_name', 'wpa', 'xCompPer', 'xCompletion', 'xYards', 'pass']]
+    # next_pred_y = xg_reg.predict(prev_act_x[['wpa', 'xCompPer', 'xCompletion', 'xYards', 'pass']])
+    # prev_act_x['pred_qb_epa'] = next_pred_y
+    # next_act_x['act_qb_epa'] = next_act_y
+    # res_df = next_act_x.merge(prev_act_x[['passer_player_id', 'qb_epa', 'pred_qb_epa']], how='left', left_on='passer_player_id', right_on='passer_player_id')
+    
+    
+    
+    
+    
+
+
+
+if __name__ == '__main__':
+    pd.set_option('mode.chained_assignment', None)
+    build_model(verbose=True)
